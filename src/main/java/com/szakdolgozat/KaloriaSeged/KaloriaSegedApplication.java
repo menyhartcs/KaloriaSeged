e933015deb87d9d21c90e230bdba34dc7b2ca8ad
Csongor Menyhart <csongor_menyhart@epam.com>
2024-05-18 15:36:34 +0200

    Initial commit

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/KaloriaSegedApplication.java
@@ -0,0 +1,13 @@
+package com.szakdolgozat.KaloriaSeged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaloriaSegedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaloriaSegedApplication.class, args);
+	}
+
+}
