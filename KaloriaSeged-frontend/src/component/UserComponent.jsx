36d6c08af27b6a69273338db76700d0a8d3959fb
Csongor Menyhart <csongor_menyhart@epam.com>
2024-05-19 16:18:31 +0200

    Add "add user" form

--- KaloriaSeged-frontend/src/component/UserComponent.jsx
+++ KaloriaSeged-frontend/src/component/UserComponent.jsx
@@ -2,8 +2,47 @@ import React from "react";
 
 const UserComponent = () => {
     return (
-        <div>
+        <div className="container">
+            <br/><br/>
+            <div className="row">
+                <div className="card col-md-6 offset-md-3 offset-md-3">
+                    <h2 className="text-center">Add User</h2>
+                    <div className="card-body">
+                        <form>
+                            <div className="form-group mb-2">
+                                <label className="form-label">Name:</label>
+                                <input type="text"
+                                       placeholder="Enter username"
+                                       name="name" value={name}
+                                       className="form-control"
+                                       onChange={(e) => setName(e.target.value)}
+                                />
+                            </div>
 
+                            <div className="form-group mb-2">
+                                <label className="form-label">Email:</label>
+                                <input type="text"
+                                       placeholder="Enter email address"
+                                       name="email" value={email}
+                                       className="form-control"
+                                       onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="form-group mb-2">
+                                <label className="form-label">Mobile:</label>
+                                <input type="text"
+                                       placeholder="Enter mobile number"
+                                       name="mobile" value={mobile}
+                                       className="form-control"
+                                       onChange={(e) => setMobile(e.target.value)}
+                                />
+                            </div>
+                            <button className="btn btn-success" onClick={saveUser}>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
