134c8132c7f75775a66cfbfa5ad4068b5eb171a5
Csongor Menyhart <csongor_menyhart@epam.com>
2024-05-19 17:21:30 +0200

    validate form

--- KaloriaSeged-frontend/src/component/UserComponent.jsx
+++ KaloriaSeged-frontend/src/component/UserComponent.jsx
@@ -1,6 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
+import {createUser} from "../service/UserService.js";
+import {useNavigate, useParams} from "react-router-dom";
 
 const UserComponent = () => {
+
+    const [name, setName] = useState([])
+    const [email, setEmail] = useState([])
+
+    const {id} = useParams();
+
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+    })
+
+    const navigator = useNavigate();
+
+    function saveUser(e) {
+        e.preventDefault();
+
+        if (validateForm()) {
+            const user = {name, email}
+            console.log(user)
+            createUser(user).then((response) => {
+                console.log(response.data)
+                navigator("/users")
+            })
+        }
+
+    }
+
+    function validateForm() {
+        let valid = true;
+
+        const errorsCopy = {... errors}
+
+        if (name.trim) {
+            errorsCopy.name = "";
+        } else {
+            errorsCopy.name = "Name is required!";
+            valid = false;
+        }
+
+        if (email.trim) {
+            errorsCopy.email = "";
+        } else {
+            errorsCopy.email = "Email address is required!";
+            valid = false;
+        }
+
+        setErrors(errorsCopy);
+
+        return valid;
+    }
+
+    function pageTitle() {
+        if (id) {
+            return <h2 className="text-center">Update User</h2>
+        } else {
+            <h2 className="text-center">Add User</h2>
+        }
+    }
+
     return (
         <div className="container">
             <br/><br/>
@@ -14,9 +75,10 @@ const UserComponent = () => {
                                 <input type="text"
                                        placeholder="Enter username"
                                        name="name" value={name}
-                                       className="form-control"
+                                       className={`form-control ${errors.name ? "is-invalid" : ""}`}
                                        onChange={(e) => setName(e.target.value)}
                                 />
+                                { errors.name && <div className="invalid-feedback">{ errors.name }</div> }
                             </div>
 
                             <div className="form-group mb-2">
@@ -24,20 +86,12 @@ const UserComponent = () => {
                                 <input type="text"
                                        placeholder="Enter email address"
                                        name="email" value={email}
-                                       className="form-control"
+                                       className={`form-control ${errors.email ? "is-invalid" : ""}`}
                                        onChange={(e) => setEmail(e.target.value)}
                                 />
+                                { errors.email && <div className="invalid-feedback">{ errors.email }</div> }
                             </div>
 
-                            <div className="form-group mb-2">
-                                <label className="form-label">Mobile:</label>
-                                <input type="text"
-                                       placeholder="Enter mobile number"
-                                       name="mobile" value={mobile}
-                                       className="form-control"
-                                       onChange={(e) => setMobile(e.target.value)}
-                                />
-                            </div>
                             <button className="btn btn-success" onClick={saveUser}>Submit</button>
                         </form>
                     </div>
