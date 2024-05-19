c81e0a06084ced73fdf9e8f38d632628034f5c78
Csongor Menyhart <csongor_menyhart@epam.com>
2024-05-19 18:00:09 +0200

    connect to delete rest api

--- KaloriaSeged-frontend/src/service/UserService.js
+++ KaloriaSeged-frontend/src/service/UserService.js
@@ -6,3 +6,4 @@ export const listUsers = () => axios.get(REST_API_BASE_URL);
 export const createUser = (user) => axios.post(REST_API_BASE_URL, user);
 export const getUser = (userId) => axios.get(REST_API_BASE_URL + "/" + userId);
 export const updateUser = (userId, user) => axios.put(REST_API_BASE_URL + "/" + userId, user);
+export const deleteUser = (userId) => axios.delete(REST_API_BASE_URL + "/" + userId);
