3db3b5d9b8b60dc19396c77e8015cc21029cd704
Csongor Menyhart <csongor_menyhart@epam.com>
2024-05-19 17:46:10 +0200

    connect to update rest api

--- KaloriaSeged-frontend/src/service/UserService.js
+++ KaloriaSeged-frontend/src/service/UserService.js
@@ -4,3 +4,5 @@ const REST_API_BASE_URL = 'http://localhost:8080/user';
 
 export const listUsers = () => axios.get(REST_API_BASE_URL);
 export const createUser = (user) => axios.post(REST_API_BASE_URL, user);
+export const getUser = (userId) => axios.get(REST_API_BASE_URL + "/" + userId);
+export const updateUser = (userId, user) => axios.put(REST_API_BASE_URL + "/" + userId, user);
