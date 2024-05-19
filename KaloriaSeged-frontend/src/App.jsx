6dbf2b9fe894b8c1714043dd03bd6c7e58ec6aaa
Csongor Menyhart <csongor_menyhart@epam.com>
2024-05-19 15:32:28 +0200

    Add header and footer

--- KaloriaSeged-frontend/src/App.jsx
+++ KaloriaSeged-frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import {useState} from 'react'
 import './App.css'
 import ListUserComponent from "./component/ListUserComponent.jsx";
+import HeaderComponent from "./component/HeaderComponent.jsx";
+import FooterComponent from "./component/FooterComponent.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
     return (
         <>
-            <ListUserComponent/>
+            <BrowserRouter>
+                <HeaderComponent/>
+                    <Routes>
+                        <Route path="/" element = { <ListUserComponent/> }></Route>
+                        <Route path="/Users" element = { <ListUserComponent/> }></Route>
+                    </Routes>
+                <FooterComponent/>
+            </BrowserRouter>
         </>
     )
 }
---
+++ KaloriaSeged-frontend/src/component/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <header>
+                <footer className="footer">
+                    <span>All rights reserved 2024 by Kalória Segéd</span>
+                </footer>
+            </header>
+        </div>
+    )
+}
+
+export default FooterComponent
