7b1d611808c303ed5ed712dec919140cf3670ef5
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-10 16:23:12 +0200

    create Food service implementation

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/service/impl/FoodServiceImpl.java
@@ -0,0 +1,63 @@
+package com.szakdolgozat.KaloriaSeged.service.impl;
+
+import com.szakdolgozat.KaloriaSeged.dto.FoodDto;
+import com.szakdolgozat.KaloriaSeged.entity.Food;
+import com.szakdolgozat.KaloriaSeged.exception.ResourceNotFoundException;
+import com.szakdolgozat.KaloriaSeged.mapper.FoodMapper;
+import com.szakdolgozat.KaloriaSeged.repository.FoodRepository;
+import com.szakdolgozat.KaloriaSeged.service.FoodService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class FoodServiceImpl implements FoodService {
+
+    private FoodRepository foodRepository;
+
+    @Override
+    public FoodDto createFood(FoodDto foodDto) {
+        Food food = FoodMapper.mapToFood(foodDto);
+        Food savedFood = foodRepository.save(food);
+        return FoodMapper.mapToFoodDto(savedFood);
+    }
+
+    @Override
+    public FoodDto getFoodById(Long foodId) {
+        Food food = foodRepository.findById(foodId)
+                .orElseThrow(() -> new ResourceNotFoundException("Food does not exist with given id: " + foodId));
+        return FoodMapper.mapToFoodDto(food);
+    }
+
+    @Override
+    public List<FoodDto> getAllFoods() {
+        List<Food> foods = foodRepository.findAll();
+        return foods.stream().map(FoodMapper::mapToFoodDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public FoodDto updateFood(Long foodId, FoodDto updatedFood) {
+        Food food = foodRepository.findById(foodId)
+                .orElseThrow(() -> new ResourceNotFoundException("Food does not exist with given id: " + foodId));
+
+        food.setName(updatedFood.getName());
+        food.setCalorie(updatedFood.getCalorie());
+        food.setFat(updatedFood.getFat());
+        food.setCarbohydrate(updatedFood.getCarbohydrate());
+        food.setProtein(updatedFood.getProtein());
+
+        Food updatedFoodObj = foodRepository.save(food);
+
+        return FoodMapper.mapToFoodDto(updatedFoodObj);
+    }
+
+    @Override
+    public void deleteFood(Long foodId) {
+        foodRepository.findById(foodId)
+                .orElseThrow(() -> new ResourceNotFoundException("Food does not exist with given id: " + foodId));
+        foodRepository.deleteById(foodId);
+    }
+}
