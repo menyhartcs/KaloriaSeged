03b48049725a7a18d2e96e55f690e88878745fab
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-10 16:22:59 +0200

    create Food repository

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/repository/FoodRepository.java
@@ -0,0 +1,7 @@
+package com.szakdolgozat.KaloriaSeged.repository;
+
+import com.szakdolgozat.KaloriaSeged.entity.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
