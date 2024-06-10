4f167f1c7b62995e8c02f55bd61dd65cf0af9428
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-10 16:56:32 +0200

    add food route paths

--- KaloriaSeged-frontend/src/App.jsx
+++ KaloriaSeged-frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
-import ListUserComponent from "./component/ListUserComponent.jsx";
 import HeaderComponent from "./component/HeaderComponent.jsx";
 import FooterComponent from "./component/FooterComponent.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import UserComponent from "./component/UserComponent.jsx";
+import ListUserComponent from "./component/ListUserComponent.jsx";
+import FoodComponent from "./component/FoodComponent.jsx";
+import ListFoodComponent from "./component/ListFoodComponent.jsx";
 
 function App() {
 
@@ -14,9 +16,12 @@ function App() {
                     <Routes>
                         <Route path="/" element = { <ListUserComponent/> }></Route>
                         <Route path="/Users" element = { <ListUserComponent/> }></Route>
-
                         <Route path="/add-user" element = { <UserComponent/> }></Route>
                         <Route path="/edit-user/:id" element = { <UserComponent/> }></Route>
+
+                        <Route path="/Foods" element = { <ListFoodComponent/> }></Route>
+                        <Route path="/add-food" element = { <FoodComponent/> }></Route>
+                        <Route path="/edit-food/:id" element = { <FoodComponent/> }></Route>
                     </Routes>
                 <FooterComponent/>
             </BrowserRouter>
