3e090df32943eb5272aa716367f715b215c2d462
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-11 17:07:54 +0200

    update Mapper classes with UserFoodLog

--- src/main/java/com/szakdolgozat/KaloriaSeged/mapper/FoodMapper.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/mapper/FoodMapper.java
@@ -1,29 +1,41 @@
 package com.szakdolgozat.KaloriaSeged.mapper;
 
 import com.szakdolgozat.KaloriaSeged.dto.FoodDto;
+import com.szakdolgozat.KaloriaSeged.dto.UserFoodLogDto;
 import com.szakdolgozat.KaloriaSeged.entity.Food;
+import com.szakdolgozat.KaloriaSeged.entity.UserFoodLog;
+
+import java.util.List;
 
 public class FoodMapper {
 
     public static FoodDto mapToFoodDto(Food food) {
+        List<UserFoodLogDto> foodLogDtos = food.getFoodLogs().stream()
+                .map(UserFoodLogMapper::mapToUserFoodLogDto)
+                .toList();
         return new FoodDto(
                 food.getId(),
                 food.getName(),
                 food.getCalorie(),
                 food.getFat(),
                 food.getCarbohydrate(),
-                food.getProtein()
+                food.getProtein(),
+                foodLogDtos
         );
     }
 
     public static Food mapToFood(FoodDto foodDto) {
+        List<UserFoodLog> foodLogs = foodDto.getFoodLogDtos().stream()
+                .map(UserFoodLogMapper::mapToUserFoodLog)
+                .toList();
         return new Food(
                 foodDto.getId(),
                 foodDto.getName(),
                 foodDto.getCalorie(),
                 foodDto.getFat(),
                 foodDto.getCarbohydrate(),
-                foodDto.getProtein()
+                foodDto.getProtein(),
+                foodLogs
         );
     }
 }
--- src/main/java/com/szakdolgozat/KaloriaSeged/mapper/UserMapper.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/mapper/UserMapper.java
@@ -1,23 +1,35 @@
 package com.szakdolgozat.KaloriaSeged.mapper;
 
 import com.szakdolgozat.KaloriaSeged.dto.UserDto;
+import com.szakdolgozat.KaloriaSeged.dto.UserFoodLogDto;
 import com.szakdolgozat.KaloriaSeged.entity.User;
+import com.szakdolgozat.KaloriaSeged.entity.UserFoodLog;
+
+import java.util.List;
 
 public class UserMapper {
 
     public static UserDto mapToUserDto(User user) {
+        List<UserFoodLogDto> userFoodLogDtos = user.getFoodLogs().stream()
+                .map(UserFoodLogMapper::mapToUserFoodLogDto)
+                .toList();
         return new UserDto(
                 user.getId(),
                 user.getName(),
-                user.getEmail()
+                user.getEmail(),
+                userFoodLogDtos
         );
     }
 
     public static User mapToUser(UserDto userDto) {
+        List<UserFoodLog> userFoodLogs = userDto.getFoodLogDtos().stream()
+                .map(UserFoodLogMapper::mapToUserFoodLog)
+                .toList();
         return new User(
                 userDto.getId(),
                 userDto.getName(),
-                userDto.getEmail()
+                userDto.getEmail(),
+                userFoodLogs
         );
     }
 }
