6e576d8905b34a11dae92009266a5c3f3b5af650
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-11 17:06:48 +0200

    create UserFoodLog

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/entity/UserFoodLog.java
@@ -0,0 +1,29 @@
+package com.szakdolgozat.KaloriaSeged.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_food_log")
+public class UserFoodLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+    private LocalDate date;
+}
