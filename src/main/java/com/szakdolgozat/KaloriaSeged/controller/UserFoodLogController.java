641dcbdd66a574f606a58e4a91baa4c8ed97641b
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-12 18:06:39 +0200

    create  UserFoodLogController

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/controller/UserFoodLogController.java
@@ -0,0 +1,50 @@
+package com.szakdolgozat.KaloriaSeged.controller;
+
+import com.szakdolgozat.KaloriaSeged.dto.UserFoodLogDto;
+import com.szakdolgozat.KaloriaSeged.service.UserFoodLogService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/userFoodLogs")
+public class UserFoodLogController {
+
+    private UserFoodLogService userFoodLogService;
+
+    @PostMapping
+    public ResponseEntity<UserFoodLogDto> createUserFoodLog(@RequestBody UserFoodLogDto userFoodLogDto) {
+        UserFoodLogDto savedUserFoodLog = userFoodLogService.createUserFoodLog(userFoodLogDto);
+        return new ResponseEntity<>(savedUserFoodLog, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UserFoodLogDto> getUserFoodLogById(@PathVariable("id") Long userFoodLogId) {
+        UserFoodLogDto userFoodLogDto = userFoodLogService.getUserFoodLogById(userFoodLogId);
+        return ResponseEntity.ok(userFoodLogDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserFoodLogDto>> getAllUserFoodLogs() {
+        List<UserFoodLogDto> userFoodLogs = userFoodLogService.getAllUserFoodLogs();
+        return ResponseEntity.ok(userFoodLogs);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<UserFoodLogDto> updateUserFoodLog(@PathVariable("id") Long userFoodLogId,
+                                                      @RequestBody UserFoodLogDto updatedUserFoodLog) {
+        UserFoodLogDto userFoodLogDto = userFoodLogService.updateUserFoodLog(userFoodLogId, updatedUserFoodLog);
+        return ResponseEntity.ok(userFoodLogDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUserFoodLog(@PathVariable("id") Long userFoodLogId) {
+        userFoodLogService.deleteUserFoodLog(userFoodLogId);
+        return ResponseEntity.ok("UserFoodLog deleted successfully");
+    }
+}
