62dc4bf2608dda0852f402ceab941d25d4dc3f08
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-15 17:27:45 +0200

    implement date field with working logic

--- KaloriaSeged-frontend/src/component/ListUserFoodLogComponent.jsx
+++ KaloriaSeged-frontend/src/component/ListUserFoodLogComponent.jsx
@@ -1,23 +1,37 @@
 import React, {useEffect, useState} from 'react'
 import {useNavigate} from "react-router-dom";
-import {deleteUserFoodLog, listUserFoodLogs} from "../service/UserFoodLogService.js";
+import {deleteUserFoodLog, listUserFoodLogs, listUserFoodLogsByDate} from "../service/UserFoodLogService.js";
 import ProteinChart from "./ProteinChart.jsx";
 import CarbohydrateChart from "./CarbohydrateChart.jsx";
 import FatChart from "./FatChart.jsx";
 import './Charts.css';
+import moment from 'moment';
 
 const ListUserFoodLogComponent = () => {
 
     const [userFoodLogs, setUserFoodLogs] = useState([])
+    const [selectedDate, setSelectedDate] = useState(getCurrentDate);
 
     const navigator = useNavigate();
 
     useEffect(() => {
-        getAllUserFoodLogs();
+        getUserFoodLogsByDate(selectedDate);
     }, []);
+
+    function getCurrentDate() {
+        return moment().format('YYYY-MM-DD');
+    }
 
     function getAllUserFoodLogs() {
         listUserFoodLogs().then((response) => {
+            setUserFoodLogs(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+    function getUserFoodLogsByDate(date) {
+        listUserFoodLogsByDate(date).then((response) => {
             setUserFoodLogs(response.data);
         }).catch(error => {
             console.error(error);
@@ -42,9 +56,31 @@ const ListUserFoodLogComponent = () => {
         })
     }
 
+    const handleChangeDate = (event) => {
+        const newDate = event.target.value;
+        setSelectedDate(newDate); // `selectedDate` állapot frissítése az új dátummal
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            getUserFoodLogsByDate(selectedDate);
+        }
+    };
+
     return (
         <div className="container">
             <h2 className="text-center">List of UserFoodLogs</h2>
+            <div className="mb-3">
+                <label htmlFor="dateInput" className="form-label">Select current date:</label>
+                <input
+                    type="date"
+                    id="dateInput"
+                    className="form-control"
+                    value={selectedDate}
+                    onChange={handleChangeDate}
+                    onKeyDown={handleKeyDown} // Esemény figyelése az Enter lenyomására
+                />
+            </div>
             <button className="btn btn-dark mb-2" onClick={addNewUserFoodLog}>Add UserFoodLog</button>
             <table className="table table-striped table-bordered">
                 <thead>
@@ -85,9 +121,12 @@ const ListUserFoodLogComponent = () => {
                             </td>
                             <td>{userFoodLog.date}</td>
                             <td>
-                                <button className="btn btn-info" onClick={() => updateUserFoodLog(userFoodLog.id)}>Update</button>
+                                <button className="btn btn-info"
+                                        onClick={() => updateUserFoodLog(userFoodLog.id)}>Update
+                                </button>
                                 <button className="btn btn-danger" onClick={() => removeUserFoodLog(userFoodLog.id)}
-                                style={{marginLeft: "10px"}}>Delete</button>
+                                        style={{marginLeft: "10px"}}>Delete
+                                </button>
                             </td>
                         </tr>)
                 }
