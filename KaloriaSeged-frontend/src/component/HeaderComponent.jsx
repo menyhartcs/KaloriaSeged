7a0c311f3f465cd6c6b41edc236521c788ac10ce
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-15 15:17:25 +0200

    add nav bar

--- KaloriaSeged-frontend/src/component/HeaderComponent.jsx
+++ KaloriaSeged-frontend/src/component/HeaderComponent.jsx
@@ -6,6 +6,8 @@ const HeaderComponent = () => {
             <header>
                 <nav className="navbar navbar-dark bg-dark">
                     <a className="navbar-brand" href="/">Kalória Segéd</a>
+                    <a className="navbar-toggler" href="/Foods">Ételek</a>
+                    <a className="navbar-toggler" href="/Users">Felhasználók</a>
                 </nav>
             </header>
         </div>
