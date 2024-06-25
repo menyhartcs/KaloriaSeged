801600c3b724df9051090484530d663f851f8560
Csongor Menyhart <csongor_menyhart@epam.com>
2024-06-25 19:37:33 +0200

    remove unused import

--- src/main/java/com/szakdolgozat/KaloriaSeged/controller/OpenAIController.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/controller/OpenAIController.java
@@ -2,15 +2,17 @@ package com.szakdolgozat.KaloriaSeged.controller;
 
 import com.szakdolgozat.KaloriaSeged.service.impl.OpenAIService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
 @CrossOrigin("*")
 @RestController
 public class OpenAIController {
-    @Autowired
+
     private OpenAIService openAIService;
 
     @PostMapping("/complete")
