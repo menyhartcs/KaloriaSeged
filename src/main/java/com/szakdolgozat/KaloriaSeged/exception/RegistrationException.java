b99b57013eeb60874dd32cfdb56fe3295d11c354
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-07 19:52:38 +0200

    create new exception class

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/exception/RegistrationException.java
@@ -0,0 +1,12 @@
+package com.szakdolgozat.KaloriaSeged.exception;
+
+public class RegistrationException extends RuntimeException {
+
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
