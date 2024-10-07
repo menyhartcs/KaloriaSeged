511e334760beea68699086513cfcce110f0fa297
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-07 19:52:07 +0200

    add findByEmail

--- src/main/java/com/szakdolgozat/KaloriaSeged/repository/UserRepository.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.szakdolgozat.KaloriaSeged.repository;
 import com.szakdolgozat.KaloriaSeged.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
