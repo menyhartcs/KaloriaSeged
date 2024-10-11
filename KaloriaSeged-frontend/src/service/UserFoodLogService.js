3785dec5d3113317d4b38960c5451c6395a05973
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-11 19:52:25 +0200

    add listUserFoodLogsByUserIdAndDate

--- KaloriaSeged-frontend/src/service/UserFoodLogService.js
+++ KaloriaSeged-frontend/src/service/UserFoodLogService.js
@@ -4,6 +4,8 @@ const REST_API_BASE_URL = 'http://localhost:8080/userFoodLog';
 
 export const listUserFoodLogs = () => axios.get(REST_API_BASE_URL);
 export const listUserFoodLogsByDate = (date) => axios.get(REST_API_BASE_URL + "/searchByDate?date="+ date);
+export const listUserFoodLogsByUserIdAndDate = (userId, date) =>
+    axios.get(REST_API_BASE_URL + "/searchByUserIdAndDate?userId=" + userId + "&" + "date=" + date);
 export const createUserFoodLog = (userFoodLog) => axios.post(REST_API_BASE_URL, userFoodLog);
 export const getUserFoodLog = (userFoodLogId) => axios.get(REST_API_BASE_URL + "/" + userFoodLogId);
 export const updateUserFoodLog = (userFoodLogId, userFoodLog) => axios.put(REST_API_BASE_URL + "/" + userFoodLogId, userFoodLog);
