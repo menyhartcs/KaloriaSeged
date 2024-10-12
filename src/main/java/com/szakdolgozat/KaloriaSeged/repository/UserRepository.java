9f401b1b101b268ea16aea7769beaadae080e6b6
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-12 16:09:49 +0200

    remove unused import

--- src/main/java/com/szakdolgozat/KaloriaSeged/repository/UserRepository.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.szakdolgozat.KaloriaSeged.repository;
 import com.szakdolgozat.KaloriaSeged.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
