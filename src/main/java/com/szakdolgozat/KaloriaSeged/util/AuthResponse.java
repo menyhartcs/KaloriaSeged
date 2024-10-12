0e145be1167d323b531b946496ae574cae8f5a69
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-12 16:10:40 +0200

    create AuthResponse

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/util/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.szakdolgozat.KaloriaSeged.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class AuthResponse {
+
+    private String token;
+}
