556e98e6764bdf44fa80dde5d8392727864ee12a
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-12 16:09:30 +0200

    create getUserByEmail method

--- src/main/java/com/szakdolgozat/KaloriaSeged/service/UserService.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/service/UserService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UserService {
     UserDto createUser(UserDto userDto);
     UserDto getUserById(Long userId);
+    UserDto getUserByEmail(String email);
     List<UserDto> getAllUsers();
     UserDto updateUser(Long userId, UserDto updatedUser);
     void deleteUser(Long userId);
