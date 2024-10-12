2daee51430d855681018fc728018b8793247c9fe
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-12 16:08:44 +0200

    create util for JWT

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/util/JWTUtil.java
@@ -0,0 +1,55 @@
+package com.szakdolgozat.KaloriaSeged.util;
+
+import com.szakdolgozat.KaloriaSeged.dto.UserDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JWTUtil {
+
+    private String SECRET_KEY = "your_secret_key";
+
+    public String generateToken(UserDto user) {
+        return Jwts.builder()
+                .setSubject(user.getName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours expiration
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    private boolean validateToken(String token, UserDto user) {
+        final String username = extractUsername(token);
+        return (username.equals(user.getName()) && !isTokenExpired(token));
+    }
+
+    private String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
