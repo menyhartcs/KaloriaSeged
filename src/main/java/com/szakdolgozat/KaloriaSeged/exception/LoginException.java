77cdc42aa1aa26b406994c316485e6ae8eb8a73a
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-13 16:55:07 +0200

    create LoginException class

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/exception/LoginException.java
@@ -0,0 +1,12 @@
+package com.szakdolgozat.KaloriaSeged.exception;
+
+public class LoginException extends RuntimeException {
+
+    public LoginException(String message) {
+        super(message);
+    }
+
+    public LoginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
