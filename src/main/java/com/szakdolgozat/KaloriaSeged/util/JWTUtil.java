a98782b75e753cdd489aed7d3d448860fa010b62
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-13 16:55:26 +0200

    fix token generation

--- src/main/java/com/szakdolgozat/KaloriaSeged/util/JWTUtil.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/util/JWTUtil.java
@@ -4,22 +4,24 @@ import com.szakdolgozat.KaloriaSeged.dto.UserDto;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
 
 @Component
 public class JWTUtil {
 
-    private String SECRET_KEY = "secret_key";
+    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(UserDto user) {
         return Jwts.builder()
                 .setSubject(user.getName())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours expiration
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
