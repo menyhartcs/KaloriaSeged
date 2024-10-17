0d5ef09a15d36cd102541def447beb074d0b6d5b
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-17 18:58:55 +0200

    front-end connected to login controller

--- KaloriaSeged-frontend/src/service/UserService.js
+++ KaloriaSeged-frontend/src/service/UserService.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const REST_API_BASE_URL = 'http://localhost:8080/user';
 const REGISTRATION_URL = 'http://localhost:8080/registration/register';
+const LOGIN_URL = 'http://localhost:8080/login';
 
 export const listUsers = () => axios.get(REST_API_BASE_URL);
-export const createUser = (user) => axios.post(REST_API_BASE_URL, user);
-export const getUser = (userId) => axios.get(REST_API_BASE_URL + "/" + userId);
 export const createUser = (user) => axios.post(REGISTRATION_URL, user);
+export const loginUser = (user) => axios.post(LOGIN_URL, user);
 export const getUserById = (userId) => axios.get(REST_API_BASE_URL + "/" + userId);
 export const getUserByEmail = (email) => axios.get(REST_API_BASE_URL + "/" + email);
 export const updateUser = (userId, user) => axios.put(REST_API_BASE_URL + "/" + userId, user);
