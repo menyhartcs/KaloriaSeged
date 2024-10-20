14ff147bdf1e740d414dff31f322363f3d92db60
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-20 16:28:47 +0200

    create UserDetailsService implementation

---
+++ src/main/java/com/szakdolgozat/KaloriaSeged/service/impl/MyUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.szakdolgozat.KaloriaSeged.service.impl;
+
+import com.szakdolgozat.KaloriaSeged.entity.User;
+import com.szakdolgozat.KaloriaSeged.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user;
+        try {
+            user = userRepository.findByEmail(email);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Felhasználó nem található: " + email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
