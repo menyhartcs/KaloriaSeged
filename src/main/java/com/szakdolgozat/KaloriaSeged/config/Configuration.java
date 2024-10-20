c19d6525874b22905a5222f50b4198bf8a718361
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-20 16:04:44 +0200

    create web security config

--- src/main/java/com/szakdolgozat/KaloriaSeged/config/Configuration.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/config/Configuration.java
@@ -2,11 +2,41 @@ package com.szakdolgozat.KaloriaSeged.config;
 
 import org.springframework.context.annotation.Bean;
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @org.springframework.context.annotation.Configuration
+@EnableWebSecurity
 public class Configuration {
     @Bean
     public BCrypt passwordEncoder() {
         return new BCrypt();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(request -> {
+            request.requestMatchers(
+                    "/registration*",
+                    "/login*").permitAll();
+            request.anyRequest().authenticated();
+        });
+
+        http.formLogin(fL -> fL.loginPage("/login")
+                .usernameParameter("email").permitAll()
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login"));
+
+        http.logout(logOut -> logOut.logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID","Idea-2e8e7cee")
+                .logoutSuccessUrl("/login"));
+
+
+        return http.build();
     }
 }
