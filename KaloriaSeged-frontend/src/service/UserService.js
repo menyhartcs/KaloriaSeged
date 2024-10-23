f558c6caf6aa1cf56ade0ce0adf38ee7c0612c64
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-23 15:31:47 +0200

    adjust login endpoint

--- KaloriaSeged-frontend/src/service/UserService.js
+++ KaloriaSeged-frontend/src/service/UserService.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const REST_API_BASE_URL = 'http://localhost:8080/user';
 const REGISTRATION_URL = 'http://localhost:8080/registration/register';
-const LOGIN_URL = 'http://localhost:8080/login';
+const LOGIN_URL = 'http://localhost:8080/api/login';
 
 export const listUsers = () => axios.get(REST_API_BASE_URL);
 export const createUser = (user) => axios.post(REGISTRATION_URL, user);
-export const loginUser = (user) => axios.post(LOGIN_URL, user);
+export const loginUser = (user) => axios.post(LOGIN_URL, user, { withCredentials: true });
 export const getUserById = (userId) => axios.get(REST_API_BASE_URL + "/id/" + userId);
 export const getUserByEmail = (email) => axios.get(REST_API_BASE_URL + "/email/" + email);
 export const updateUser = (userId, user) => axios.put(REST_API_BASE_URL + "/" + userId, user);
