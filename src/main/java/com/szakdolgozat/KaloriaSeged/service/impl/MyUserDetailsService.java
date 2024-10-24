9cc6539ef3e1a37e39a70015a18b263fd5d03241
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-24 20:26:42 +0200

    remove import

--- src/main/java/com/szakdolgozat/KaloriaSeged/service/impl/MyUserDetailsService.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/service/impl/MyUserDetailsService.java
@@ -3,7 +3,6 @@ package com.szakdolgozat.KaloriaSeged.service.impl;
 import com.szakdolgozat.KaloriaSeged.entity.User;
 import com.szakdolgozat.KaloriaSeged.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
