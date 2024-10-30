f6e1cc95639115011e132b16fa76035f8a8444a4
Csongor Menyhart <csongor_menyhart@epam.com>
2024-10-30 19:56:15 +0100

    add Napló to navbar

--- KaloriaSeged-frontend/src/component/HeaderComponent.jsx
+++ KaloriaSeged-frontend/src/component/HeaderComponent.jsx
@@ -6,6 +6,7 @@ const HeaderComponent = () => {
             <header>
                 <nav className="navbar navbar-dark bg-dark">
                     <a className="navbar-brand" href="/">Kalória Segéd</a>
+                    <a className="navbar-toggler" href="/UserFoodLogs">Napló</a>
                     <a className="navbar-toggler" href="/Foods">Ételek</a>
                     <a className="navbar-toggler" href="/Users">Felhasználók</a>
                     <a className="navbar-toggler" href="/UserLogIn">Bejelentkezés/Regisztráció</a>
