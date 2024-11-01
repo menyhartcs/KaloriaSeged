e8bfa133549ffb179542beaf862a506f72fc7b08
Csongor Menyhart <csongor_menyhart@epam.com>
2024-11-01 11:33:55 +0100

    add dynamic header change

--- KaloriaSeged-frontend/src/component/HeaderComponent.jsx
+++ KaloriaSeged-frontend/src/component/HeaderComponent.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {isNullOrUndef} from "chart.js/helpers";
 
 const HeaderComponent = () => {
+    const [email, setEmail] = useState(localStorage.getItem("email"));
+    const [token, setToken] = useState(localStorage.getItem("token"));
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const newEmail = localStorage.getItem("email");
+            const newToken = localStorage.getItem("token");
+
+            // Csak akkor állítjuk be az állapotot, ha az új értékek különböznek a régiektől
+            if (newEmail !== email) setEmail(newEmail);
+            if (newToken !== token) setToken(newToken);
+        }, 100); // Ellenőrizzük 1 másodpercenként
+
+        return () => clearInterval(intervalId); // Tisztítjuk az időzítőt, ha a komponens eltávolításra kerül
+    }, [email, token]);
+
+
+    function showLoginOrLogout() {
+        console.log("email: ", email)
+        console.log("token: ", token)
+        if (isNullOrUndef(email) && isNullOrUndef(token)) {
+            return <a className="navbar-toggler" href="/UserLogIn">Bejelentkezés/Regisztráció</a>
+        }
+        return <a className="navbar-toggler" href="/UserLogOut">Kijelentkezés</a>
+    }
+
     return (
         <div>
             <header>
@@ -9,7 +36,7 @@ const HeaderComponent = () => {
                     <a className="navbar-toggler" href="/UserFoodLogs">Napló</a>
                     <a className="navbar-toggler" href="/Foods">Ételek</a>
                     <a className="navbar-toggler" href="/Users">Felhasználók</a>
-                    <a className="navbar-toggler" href="/UserLogIn">Bejelentkezés/Regisztráció</a>
+                    {showLoginOrLogout()}
                 </nav>
             </header>
         </div>
