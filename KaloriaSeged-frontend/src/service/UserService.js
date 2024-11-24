b9b9faa379306867eee7590b76cfa23c36f39c95
Csongor Menyhart <csongor_menyhart@epam.com>
2024-11-24 16:08:35 +0100

    fix getUserById param

--- KaloriaSeged-frontend/src/service/UserService.js
+++ KaloriaSeged-frontend/src/service/UserService.js
@@ -21,7 +21,7 @@ const LOGIN_URL = '/api/login';
 export const listUsers = () => axiosClient.get(REST_API_BASE_URL)
 export const createUser = (user) => axiosClient.post(REGISTRATION_URL, user);
 export const loginUser = (user) => axiosClient.post(LOGIN_URL, user, { withCredentials: true });
-export const getUserById = (userId) => axiosClient.get({REST_API_BASE_URL} + "/id/" + userId);
+export const getUserById = (userId) => axiosClient.get(REST_API_BASE_URL + "/id/" + userId);
 export const getUserByEmail = (email) => axiosClient.get(REST_API_BASE_URL + "/email/" + email);
 export const updateUser = (userId, user) => axiosClient.put(REST_API_BASE_URL + "/" + userId, user);
 export const deleteUser = (userId) => axiosClient.delete(REST_API_BASE_URL + "/" + userId);
