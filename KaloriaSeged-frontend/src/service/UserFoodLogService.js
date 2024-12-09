546cf4e1a6cc6f746210a865f39e2de55502b1d4
Csongor Menyhart <csongor_menyhart@epam.com>
2024-12-09 19:09:30 +0100

    using http only cookie setting on front-end

--- KaloriaSeged-frontend/src/service/UserFoodLogService.js
+++ KaloriaSeged-frontend/src/service/UserFoodLogService.js
@@ -2,16 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
     baseURL: 'http://localhost:8080',
-});
-
-axiosClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
+    withCredentials: true
 });
 
 const REST_API_BASE_URL = '/userFoodLog';
--- KaloriaSeged-frontend/src/service/UserService.js
+++ KaloriaSeged-frontend/src/service/UserService.js
@@ -2,16 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
     baseURL: 'http://localhost:8080',
-});
-
-axiosClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
+    withCredentials: true
 });
 
 const REST_API_BASE_URL = '/user';
@@ -20,7 +11,8 @@ const LOGIN_URL = '/api/login';
 
 export const listUsers = () => axiosClient.get(REST_API_BASE_URL)
 export const createUser = (user) => axiosClient.post(REGISTRATION_URL, user);
-export const loginUser = (user) => axiosClient.post(LOGIN_URL, user, { withCredentials: true });
+export const loginUser = (user) => axiosClient.post(LOGIN_URL, user);
+export const logOutUser = () => axiosClient.post("/logout");
 export const getUserById = (userId) => axiosClient.get(REST_API_BASE_URL + "/id/" + userId);
 export const getUserByEmail = (email) => axiosClient.get(REST_API_BASE_URL + "/email/" + email);
 export const updateUser = (userId, user) => axiosClient.put(REST_API_BASE_URL + "/" + userId, user);
