56fc2476ac6f03c65b3681e54cfafe84e9c76eed
Csongor Menyhart <csongor_menyhart@epam.com>
2024-12-09 17:20:19 +0100

    add role restrictions to UserFoodLog and UserExerciseLog controllers

--- src/main/java/com/szakdolgozat/KaloriaSeged/controller/UserExerciseLogController.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/controller/UserExerciseLogController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -25,6 +26,7 @@ public class UserExerciseLogController {
 
     // Handles the POST request for create UserExerciseLog.
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<UserExerciseLogDto> createUserExerciseLog(@RequestBody UserExerciseLogDto userExerciseLogDto) {
         UserExerciseLogDto savedUserExerciseLog = userExerciseLogService.createUserExerciseLog(userExerciseLogDto);
         return new ResponseEntity<>(savedUserExerciseLog, HttpStatus.CREATED);
@@ -32,6 +34,7 @@ public class UserExerciseLogController {
 
     // Handles the GET request for find UserExerciseLog by id.
     @GetMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<UserExerciseLogDto> getUserExerciseLogById(@PathVariable("id") Long userExerciseLogId) {
         UserExerciseLogDto userExerciseLogDto = userExerciseLogService.getUserExerciseLogById(userExerciseLogId);
         return ResponseEntity.ok(userExerciseLogDto);
@@ -39,6 +42,7 @@ public class UserExerciseLogController {
 
     // Handles the GET request for find UserExerciseLog by User id.
     @GetMapping("/searchByUserId")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserExerciseLogDto>> getUserExerciseLogByUserId(@RequestParam Long userId) {
         List<UserExerciseLogDto> userExerciseLogDto = userExerciseLogService.getUserExerciseLogsByUserId(userId);
         return ResponseEntity.ok(userExerciseLogDto);
@@ -46,6 +50,7 @@ public class UserExerciseLogController {
 
     // Handles the GET request for find UserExerciseLog by User id and date.
     @GetMapping("/searchByUserIdAndDate")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserExerciseLogDto>> getUserExerciseLogByDate(@RequestParam Long userId,
                                                                      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         List<UserExerciseLogDto> userExerciseLogDto = userExerciseLogService.getUserExerciseLogsByUserIdAndDate(userId, date);
@@ -54,6 +59,7 @@ public class UserExerciseLogController {
 
     // Handles the GET request for find UserExerciseLog by date.
     @GetMapping("/searchByDate")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserExerciseLogDto>> getUserExerciseLogByDate(
                                          @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         List<UserExerciseLogDto> userExerciseLogDto = userExerciseLogService.getUserExerciseLogsByDate(date);
@@ -62,6 +68,7 @@ public class UserExerciseLogController {
 
     // Handles the GET request for find all the UserExerciseLogs.
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserExerciseLogDto>> getAllUserExerciseLogs() {
         List<UserExerciseLogDto> userExerciseLogs = userExerciseLogService.getAllUserExerciseLogs();
         return ResponseEntity.ok(userExerciseLogs);
@@ -69,6 +76,7 @@ public class UserExerciseLogController {
 
     // Handles the PUT request for update UserExerciseLog.
     @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<UserExerciseLogDto> updateUserExerciseLog(@PathVariable("id") Long userExerciseLogId,
                                                       @RequestBody UserExerciseLogDto updatedUserExerciseLog) {
         UserExerciseLogDto userExerciseLogDto = userExerciseLogService.updateUserExerciseLog(userExerciseLogId, updatedUserExerciseLog);
@@ -77,6 +85,7 @@ public class UserExerciseLogController {
 
     // Handles the DELETE request for delete UserExerciseLog.
     @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<String> deleteUserExerciseLog(@PathVariable("id") Long userExerciseLogId) {
         userExerciseLogService.deleteUserExerciseLog(userExerciseLogId);
         return ResponseEntity.ok("UserExerciseLog deleted successfully");
--- src/main/java/com/szakdolgozat/KaloriaSeged/controller/UserFoodLogController.java
+++ src/main/java/com/szakdolgozat/KaloriaSeged/controller/UserFoodLogController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -25,6 +26,7 @@ public class UserFoodLogController {
 
     // Handles the POST request for create UserFoodLog.
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<UserFoodLogDto> createUserFoodLog(@RequestBody UserFoodLogDto userFoodLogDto) {
         UserFoodLogDto savedUserFoodLog = userFoodLogService.createUserFoodLog(userFoodLogDto);
         return new ResponseEntity<>(savedUserFoodLog, HttpStatus.CREATED);
@@ -32,6 +34,7 @@ public class UserFoodLogController {
 
     // Handles the GET request for find UserFoodLog by id.
     @GetMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<UserFoodLogDto> getUserFoodLogById(@PathVariable("id") Long userFoodLogId) {
         UserFoodLogDto userFoodLogDto = userFoodLogService.getUserFoodLogById(userFoodLogId);
         return ResponseEntity.ok(userFoodLogDto);
@@ -39,6 +42,7 @@ public class UserFoodLogController {
 
     // Handles the GET request for find UserFoodLog by User id.
     @GetMapping("/searchByUserId")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserFoodLogDto>> getUserFoodLogByUserId(@RequestParam Long userId) {
         List<UserFoodLogDto> userFoodLogDto = userFoodLogService.getUserFoodLogsByUserId(userId);
         return ResponseEntity.ok(userFoodLogDto);
@@ -46,6 +50,7 @@ public class UserFoodLogController {
 
     // Handles the GET request for find UserFoodLog by User id and date.
     @GetMapping("/searchByUserIdAndDate")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserFoodLogDto>> getUserFoodLogByDate(@RequestParam Long userId,
                                                                      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         List<UserFoodLogDto> userFoodLogDto = userFoodLogService.getUserFoodLogsByUserIdAndDate(userId, date);
@@ -54,6 +59,7 @@ public class UserFoodLogController {
 
     // Handles the GET request for find UserFoodLog by date.
     @GetMapping("/searchByDate")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserFoodLogDto>> getUserFoodLogByDate(
                                          @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         List<UserFoodLogDto> userFoodLogDto = userFoodLogService.getUserFoodLogsByDate(date);
@@ -62,6 +68,7 @@ public class UserFoodLogController {
 
     // Handles the GET request for find all the UserFoodLogs.
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<List<UserFoodLogDto>> getAllUserFoodLogs() {
         List<UserFoodLogDto> userFoodLogs = userFoodLogService.getAllUserFoodLogs();
         return ResponseEntity.ok(userFoodLogs);
@@ -69,6 +76,7 @@ public class UserFoodLogController {
 
     // Handles the PUT request for update UserFoodLog.
     @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<UserFoodLogDto> updateUserFoodLog(@PathVariable("id") Long userFoodLogId,
                                                       @RequestBody UserFoodLogDto updatedUserFoodLog) {
         UserFoodLogDto userFoodLogDto = userFoodLogService.updateUserFoodLog(userFoodLogId, updatedUserFoodLog);
@@ -77,6 +85,7 @@ public class UserFoodLogController {
 
     // Handles the DELETE request for delete UserFoodLog.
     @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<String> deleteUserFoodLog(@PathVariable("id") Long userFoodLogId) {
         userFoodLogService.deleteUserFoodLog(userFoodLogId);
         return ResponseEntity.ok("UserFoodLog deleted successfully");
